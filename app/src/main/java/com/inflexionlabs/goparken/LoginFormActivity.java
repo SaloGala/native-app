e8c24a4f33d958a2c88ab3a1d554e8aff9cc8288
Salomón Pérez <s.perez@ubroapps.com>
2017-08-13 03:38:55 -0500

    Finishes the login screen

--- app/src/main/java/com/inflexionlabs/goparken/LoginFormActivity.java
+++ app/src/main/java/com/inflexionlabs/goparken/LoginFormActivity.java
@@ -1,28 +1,30 @@
 package com.inflexionlabs.goparken;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
-import android.support.constraint.solver.widgets.ConstraintAnchor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.*;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginFormActivity extends AppCompatActivity {
 
@@ -38,11 +40,15 @@ public class LoginFormActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("NexaLight.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
         setContentView(R.layout.activity_login_form);
+
 
         emailField = (EditText) findViewById(R.id.txtEmail);
         passwordField = (EditText) findViewById(R.id.txtPass);
@@ -58,13 +64,12 @@ public class LoginFormActivity extends AppCompatActivity {
     private void initializeComponents() {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
-        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
         final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
-        upArrow.setColorFilter(getResources().getColor(R.color.colorPrimaryActionBar), PorterDuff.Mode.SRC_ATOP);
+        upArrow.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
@@ -83,19 +88,19 @@ public class LoginFormActivity extends AppCompatActivity {
 
 
         // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email,password)
+        mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Log.d(TAG,"signinWithEmail:success");
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signinWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
 
-                            registerEventFirebaseAnalitics("email_password","El usuario inicio sesion con un correo y contraseña");
+                            registerEventFirebaseAnalitics("email_password", "El usuario inicio sesion con un correo y contraseña");
 
                             goMapScreen();
 
-                        }else{
+                        } else {
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(LoginFormActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                         }
@@ -111,7 +116,7 @@ public class LoginFormActivity extends AppCompatActivity {
 
         String email = emailField.getText().toString();
         if (TextUtils.isEmpty(email)) {
-            emailField.setError("Required.");
+            emailField.setError("Campo requerido");
             valid = false;
         } else {
             emailField.setError(null);
@@ -119,7 +124,7 @@ public class LoginFormActivity extends AppCompatActivity {
 
         String password = passwordField.getText().toString();
         if (TextUtils.isEmpty(password)) {
-            passwordField.setError("Required.");
+            passwordField.setError("Campo requerido");
             valid = false;
         } else {
             passwordField.setError(null);
@@ -128,26 +133,31 @@ public class LoginFormActivity extends AppCompatActivity {
         return valid;
     }
 
-    private void goMapScreen(){
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    private void goMapScreen() {
         Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.btnLogIn) {
-            signIn(emailField.getText().toString(),passwordField.getText().toString());
+            signIn(emailField.getText().toString(), passwordField.getText().toString());
         }
     }
 
-    public void registerEventFirebaseAnalitics(String name , String description){
+    public void registerEventFirebaseAnalitics(String name, String description) {
 
         Bundle params = new Bundle();
-        params.putString("nombre",name);
-        params.putString("descripcion",description);
+        params.putString("nombre", name);
+        params.putString("descripcion", description);
 
-        mFirebaseAnalytics.logEvent("inicio_sesion",params);
+        mFirebaseAnalytics.logEvent("inicio_sesion", params);
 
     }
 }
