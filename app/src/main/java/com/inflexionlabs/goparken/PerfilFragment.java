516eb59cbe6a808add6e2a18ea0d09117dea08ff
Salomón Pérez <s.perez@ubroapps.com>
2017-08-26 03:29:25 -0500

    Fixes map reload issue

--- app/src/main/java/com/inflexionlabs/goparken/PerfilFragment.java
+++ app/src/main/java/com/inflexionlabs/goparken/PerfilFragment.java
@@ -3,6 +3,7 @@ package com.inflexionlabs.goparken;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +11,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 /**
  * Created by odalysmarronsanchez on 23/08/17.
  */
 
-public class PerfilFragment extends Fragment{
+public class PerfilFragment extends Fragment {
 
     public View mView;
 
@@ -28,12 +36,16 @@ public class PerfilFragment extends Fragment{
     Button btnEditarPerfil;
     Button btnAddCard;
     Button btnAddAuto;
+    private DatabaseReference mDatabaseReference;
+    FirebaseUser mFirebaseUser;
+    private FirebaseAuth mFirebaseAuth;
+
 
     String photoUrl;
 
     UserUtilities userUtilities = UserUtilities.getInstance();
 
-    public PerfilFragment(){
+    public PerfilFragment() {
 
     }
 
@@ -59,23 +71,81 @@ public class PerfilFragment extends Fragment{
         btnEditarPerfil.setOnClickListener(btnListener);
         btnAddAuto.setOnClickListener(btnListener);
         btnAddCard.setOnClickListener(btnListener);
-        showInfoUser();
-
+        initializeUserInfo();
         return mView;
     }
 
-    private void showInfoUser(){
+    private void initializeUserInfo() {
 
-        if(userUtilities.getProvider().equals("password")){
-            photoUrl = "https://firebasestorage.googleapis.com/v0/b/goparkennativa-cfff1.appspot.com/o/perfil_imagen%402x.png?alt=media&token=0104417e-f8d8-4b1d-8712-ea90e18ecadd";
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-        }else{
-            photoUrl = userUtilities.getPhotoUrl().toString();
-        }
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
-        txtUserName.setText(userUtilities.getUserName());
-        txtUserEmail.setText(userUtilities.getEmail());
-        Picasso.with(getContext()).load(photoUrl).fit().into(imgUserPhoto);
+        //Traer de la base
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        DatabaseReference mUserDetail = mDatabaseReference.child("users").child(mFirebaseUser.getUid() + "/data");
+
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                userUtilities.setId(user.getId());
+                userUtilities.setUid(user.getUid());
+                userUtilities.setUserName(user.getUserName());
+                userUtilities.setEmail(user.getEmail());
+                userUtilities.setPassword(user.getPassword());
+                userUtilities.setToken(user.getToken());
+                userUtilities.setStatus(user.getStatus());
+                userUtilities.setType(user.getType());
+                userUtilities.setAccess_token(user.getAccess_token());
+                userUtilities.setNickname(user.getNickname());
+                userUtilities.setFull_name(user.getNickname());
+                userUtilities.setAvatar(user.getAvatar());
+                userUtilities.setDetails(user.getDetails());
+                userUtilities.setSocial_id(user.getSocial());
+                userUtilities.setSocial_type(user.getSocial_type());
+                userUtilities.setSocial_id(user.getSocial_id());
+                userUtilities.setSocial_json(user.getSocial_json());
+                userUtilities.setSocial_email(user.getSocial_email());
+                userUtilities.setLastname(user.getLastname());
+                userUtilities.setPhone(user.getPhone());
+                userUtilities.setPostalcode(user.getPostalcode());
+                userUtilities.setState(user.getState());
+                userUtilities.setCity(user.getCity());
+                userUtilities.setOpenpay_id(user.getOpenpay_id());
+                userUtilities.setRemember_token(user.getRemember_token());
+                userUtilities.setAddress(user.getAddress());
+                userUtilities.setFacebook_share(user.getFacebook_share());
+                userUtilities.setProvider(user.getProvider());
+                userUtilities.setPhotoUrl(user.getPhotoUrl());
+
+
+                if (userUtilities.getProvider().equals("password")) {
+                    photoUrl = "https://firebasestorage.googleapis.com/v0/b/goparkennativa-cfff1.appspot.com/o/perfil_imagen%402x.png?alt=media&token=0104417e-f8d8-4b1d-8712-ea90e18ecadd";
+
+                } else {
+                    photoUrl = userUtilities.getPhotoUrl().toString();
+                }
+
+                txtUserName.setText(userUtilities.getUserName());
+                txtUserEmail.setText(userUtilities.getEmail());
+                Picasso.with(getContext()).load(photoUrl).fit().into(imgUserPhoto);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+
+
+        };
+
+        mUserDetail.addListenerForSingleValueEvent(userListener);
+
+
     }
 
     private View.OnClickListener btnListener = new View.OnClickListener() {
@@ -108,13 +178,13 @@ public class PerfilFragment extends Fragment{
         startActivity(intent);
     }
 
-    private void addAuto(){
-        Intent intent = new Intent(getActivity(),AddVehicleActivity.class);
+    private void addAuto() {
+        Intent intent = new Intent(getActivity(), AddVehicleActivity.class);
         startActivity(intent);
     }
 
-    private void showCardsList(){
-        Intent intent = new Intent(getActivity(),CardsListActivity.class);
+    private void showCardsList() {
+        Intent intent = new Intent(getActivity(), CardsListActivity.class);
         startActivity(intent);
     }
 
